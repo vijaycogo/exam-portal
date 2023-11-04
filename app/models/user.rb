93f7500b7701085d98bbe6a8f30214e30817ae0b
vijaycogo <vijay.kumar@cogoport.com>
2023-11-04 11:06:51 +0530

    add validaion on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,20 @@
-# app/models/user.rb
 class User < ApplicationRecord
   belongs_to :college
   belongs_to :exam
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true, length: { is: 10, message: "should be 10 characters long" }
+  validates :college_id, presence: true
+  validates :exam_id, presence: true
+  validates :start_time, presence: true
+
+  validate :validate_mobile_number, if: Proc.new { |t| t.phone_number.present? }
+  private
+
+  def validate_mobile_number
+    unless phone_number.match(/\A\d{10}\z/)    
+      errors.add(:phone_number, "is not in the correct format")
+    end
+  end
 end
