8abaa9db4dd009db4d141fd2ac4808d3cd00d4e0
vijaycogo <vijay.kumar@cogoport.com>
2023-11-04 10:41:30 +0530

    generate schema

---
+++ db/schema.rb
@@ -0,0 +1,64 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2023_11_03_221136) do
+
+  create_table "colleges", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "exam_windows", force: :cascade do |t|
+    t.integer "exam_id", null: false
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["exam_id"], name: "index_exam_windows_on_exam_id"
+  end
+
+  create_table "exams", force: :cascade do |t|
+    t.integer "college_id", null: false
+    t.string "exam_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["college_id"], name: "index_exams_on_college_id"
+  end
+
+  create_table "request_response_logs", force: :cascade do |t|
+    t.text "request_params"
+    t.text "response"
+    t.datetime "request_time"
+    t.integer "status_code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
+    t.integer "college_id", null: false
+    t.integer "exam_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "start_time"
+    t.index ["college_id"], name: "index_users_on_college_id"
+    t.index ["exam_id"], name: "index_users_on_exam_id"
+  end
+
+  add_foreign_key "exam_windows", "exams"
+  add_foreign_key "exams", "colleges"
+  add_foreign_key "users", "colleges"
+  add_foreign_key "users", "exams"
+end
